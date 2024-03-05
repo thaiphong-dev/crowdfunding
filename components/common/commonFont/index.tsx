764f8e1a3a138ce5b4ab4e43b799011f78e0609d
Từ Thái Phong <tuthaiphong701@gmail.com>
2024-03-06 03:13:42 +0700

    remove fallback font

--- components/common/commonFont/index.tsx
+++ components/common/commonFont/index.tsx
@@ -2,22 +2,18 @@ import { Sue_Ellen_Francisco, Shippori_Mincho } from "next/font/google";
 const sueFont = Sue_Ellen_Francisco({
   weight: "400",
   subsets: ["latin"],
-  adjustFontFallback: false,
 });
 const shipporiFont = Shippori_Mincho({
   weight: "800",
   subsets: ["latin"],
-  adjustFontFallback: false,
 });
 const shipporiFont600 = Shippori_Mincho({
   weight: "600",
   subsets: ["latin"],
-  adjustFontFallback: false,
 });
 const shipporiFont700 = Shippori_Mincho({
   weight: "700",
   subsets: ["latin"],
-  adjustFontFallback: false,
 });
 export const CustomFont = {
   sueFont,
